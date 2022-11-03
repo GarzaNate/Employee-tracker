4c98d6e53c2b976a39ee6b9602fa1a5049596121
slendyG <josuegarza0608@gmail.com>
2022-11-02 21:53:19 -0700

    lots of changes to server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const db = mysql.createConnection({
     database: 'employee_db'
 });
 
+
 // if db doesn't connect, throw error
 db.connect(err => {
     if (err) throw err;
@@ -37,13 +38,13 @@ const initialQuestion = () => {
                 viewEmployees();
                 break;
             case "Add a department":
-                addNewDepartment();
+                addDepartment();
                 break;
             case "Add a role":
-                addNewRole();
+                addRole();
                 break;
             case "Add an employee":
-                addNewEmployee();
+                addEmployee();
                 break;
             case "Update role for an employee":
                 updateRole();
@@ -53,34 +54,119 @@ const initialQuestion = () => {
                 break;
             default:
                 break;
-        }
-    })
-}
+        };
+    });
+};
 
 // View Departments
 const viewDepartments = () => {
     console.log("Department View");
 
-}
+    var sql = 'SELECT department.id AS id, department.name AS department FROM department';
+    db.query(sql, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        initialQuestion();
+    });
+};
 
 // View Roles
 const viewRoles = () => {
     console.log("Role View");
 
-}
+    var sql = 'SELECT role.id, role.title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department_id';
+    db.query(sql, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        initialQuestion();
+    });
+};
 
 // View Employees
 const viewEmployees = () => {
     console.log("Employee View");
 
+    var sql = 'SELECT e.first_name, e.last_name, r.title, r.salary, d.name, CONCAT(manager.first_name, " ", manager.last_name) AS manager_name FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r department_id = d.id LEFT JOIN employee manager ON manager.id = e.manager_id ORDER BY salary DESC';
+    db.query(sql, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        initialQuestion();
+    });
 }
 
 // Add Department
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newDepartment',
+            message: 'Enter the name of the new department',
+        }
+    ]).then(answer => {
+        var sql = 'INSERT INTO department (name) VALUES (?)';
 
+        connection.query(sql, answer.addDepartment, (err, res) => {
+            if (err) throw err;
+            console.log(answer.addDepartment + ' was added as a new department');
+            console.table('All Departments');
+
+            viewDepartments();
+            initialQuestion();
+        });
+    });
+};
 
 // Add Role
 
+const addRole = () => {
+   
+    const role = 'SELECT id, name FROM department';
+
+    connection.query(role, (err, res) => {
+        if (err) throw err;
+
+        const department = res.map(({ id, name }) => ({ value: id, name: name }));
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newRole',
+                message: 'Enter new role'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'Please enter the salary for this role'
+            },
+            {
+                type: 'list',
+                name: 'dept',
+                message: 'Select the department for this role',
+                choices: department
+            }
+        ]).then(answer => {
+            const input = [answer.role, answer.salary, answer.dept]
+
+            const sql = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);'
+
+            connection.query(sql, input, (err, res) => {
+                if (err) throw err;
+                console.log('Added ' + answer.role + ' to roles');
+                viewRoles();
+            });
+        });
+    });
+};
 
 // Add Employee
+
+const addEmployee= () => {
+
+    connection.query('SELECT * from role', (err, res) => {
+        const roles = res.map(({ id, title }) => ({ value: id, name: title }))
+
+        
+}
+
 
 // Update Employee Role
